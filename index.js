fd91e114ba0156bdc5a3caae93845c43486d5ce8
faheem92mt <faheemibnhabib@gmail.com>
2024-06-14 23:30:14 +0800

    comment

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const appSettings = {
+    databaseURL: "https://playground-c1998-default-rtdb.firebaseio.com/"
+}
+
+import {add} from "./functions.js"
+
+console.log(add(1,1));
+
 var button = document.querySelector("button")
 var input = document.querySelector("input")
 
