e9fb73369652ffe39ad018bcaba3e37d9250ac11
faheem92mt <faheemibnhabib@gmail.com>
2024-06-16 15:23:40 +0800

    comment

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+// importing the method uesed to initialize the app
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+// import the 'getDatabase'
+import {getDatabase} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+// defining our app settings
 const appSettings = {
     databaseURL: "https://playground-c1998-default-rtdb.firebaseio.com/"
 }
 
-import {add} from "./functions.js"
+// defining our app
+const app = initializeApp(appSettings)
 
-console.log(add(1,1));
+console.log(app);
+
+// import {add} from "./functions.js"
+// console.log(add(1,1));
 
 var button = document.querySelector("button")
 var input = document.querySelector("input")
